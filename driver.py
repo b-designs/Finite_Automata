92c00e82fa6f7fed865a15d389070f6dd08029f9
Basilio Lopez <blopezdesigns@gmail.com>
2024-10-16 17:33:31 -0700

    Added a gather states function that only stores singular states. However, it will need an implementation to gather states such as (q0,q1) vs (q0)

--- driver.py
+++ driver.py
@@ -1,7 +1,8 @@
 
 inputs = ["d(q0,a)={q0,q1},d(q1,b)={q1,q2},d(q2,a)={q2}",
-          "d(q0,a)={q0,q1},d(q1,b)={q1,q2},d(q2,a)={q2},d(q0,lambda)"
-          ]
+          "d(q0,a)={q0,q1},d(q1,b)={q1,q2},d(q2,a)={q2},d(q0,lambda)"]
+
+states = []
 
 # Prints all inputs
 def print_input(str):
@@ -11,8 +12,26 @@ def print_input(str):
             print(str[index])
         else:
             print(str[index], end="")
-
-
-
+            
+# Adds states from a string and stores them in the states list     
+def gather_states(str):
+    global states
+    for index in range(0, len(str)):
+        if str[index-1] == "q":
+            states.append("q" + str[index])
+    
+    # Removes an repeats
+    dummyvar = set(states)
+    states = dummyvar
+    
+    # Sorts the states
+    dummyvar = sorted(states)
+    states = dummyvar
+            
+    # Prints the states
+    print("\n\n~ States gathered: ~")
+    print(states)
+    
+            
 print_input(inputs[1])
-        
+gather_states(inputs[1])        
